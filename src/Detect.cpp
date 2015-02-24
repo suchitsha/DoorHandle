9a6ff11ac388f9ce3c8e4ade02df1b2e0343228f
u2 <u2@u2.(none)>
2015-02-24 11:14:25 +0100

    updated

--- src/Detect.cpp
+++ src/Detect.cpp
@@ -19,17 +19,19 @@
 #include <pcl/kdtree/kdtree.h>
 #include <pcl/segmentation/extract_clusters.h>
 
-//todo
+//TODO remove this when not needed
 #include <pcl/visualization/pcl_visualizer.h>
 using namespace pcl;
 
 pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr Detect::startDetection(
 		const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr &cloud) {
-	    
-	    //TODO remove this if not used
-    	//create and fill input from XYZRGBA to xyz cloud 
-	    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloudXYZRGBA (new pcl::PointCloud<pcl::PointXYZRGBA>);
-	    *cloudXYZRGBA = *cloud;
+	    //for filtering normal
+	    pcl::PointCloud<pcl::Normal>::Ptr cloudNormalFiltered (new pcl::PointCloud<pcl::Normal>);
+	    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloudFiltered (new pcl::PointCloud<pcl::PointXYZRGBA>);
+	    //for segmentation
+	    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloudPlane(new pcl::PointCloud<pcl::PointXYZRGBA>);
+	    //pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloudOnPlane(new pcl::PointCloud<pcl::PointXYZRGBA>);
+	    pcl::ExtractIndices<pcl::PointXYZRGBA> extract; 
 	    
 	    //-----------compute normals------------------//
 	     
@@ -40,18 +42,74 @@ pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr Detect::startDetection(
 	    pcl::search::KdTree<pcl::PointXYZRGBA>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGBA> ());
 	    ne.setSearchMethod (tree);
 		// Output datasets
-	    pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+	    pcl::PointCloud<pcl::Normal>::Ptr cloudNormals (new pcl::PointCloud<pcl::Normal>);
 	    // Use all neighbors in a sphere of radius 3cm
-	    ne.setRadiusSearch (0.03);
+	    ne.setRadiusSearch (0.01); //TODO put to constants
 		// Compute the features
-	    ne.compute (*cloud_normals);
+	    ne.compute (*cloudNormals);
 	    
 	    
 	    //-----filter out the normals from the ground, ceilling,etc-----//
-	    
-	    for(pcl::PointCloud<pcl::Normal>::iterator it = cloud_normals->begin(); it!= cloud_normals->end(); it++){
-        cout << "a" << it << endl;
-    	}
+	    pcl::PointCloud<pcl::PointXYZRGBA>::const_iterator itCloud = cloud->begin();
+	    for(pcl::PointCloud<pcl::Normal>::iterator it = cloudNormals->begin(); it!= cloudNormals->end(); it++){
+			
+			//check if normal is nearly parallel to z axis ie dot product is > cos(theta)
+			float dotProductZ = it->normal_x*0 + it->normal_y*0 + it->normal_z *1;
+			float angle = 0.97; //TODO put to constants
+			
+			//change sign as we only need the magnitude here
+			if (dotProductZ < 0)
+			{
+				dotProductZ = -dotProductZ;
+			}
+			if(dotProductZ > angle){	 
+				pcl::Normal norm (it->normal_x,it->normal_y,it->normal_z);
+				cloudNormalFiltered->push_back(norm);
+
+				pcl::PointXYZRGBA point ;
+				point.x = itCloud->x;
+				point.y = itCloud->y;
+				point.z = itCloud->z;
+				point.rgba = itCloud->rgba;
+				cloudFiltered->push_back(point);				 	
+				
+			}
+			itCloud++;
+			
+		}
+		
+		//--------------do segmentation------------//
+		pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+		pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+		// Create the segmentation object
+		pcl::SACSegmentation<pcl::PointXYZRGBA> seg;
+		// Optional
+		seg.setOptimizeCoefficients (true);
+		// Mandatory
+		seg.setModelType (pcl::SACMODEL_PLANE);
+		seg.setMethodType (pcl::SAC_RANSAC);
+		seg.setDistanceThreshold (0.01);
+
+		seg.setInputCloud (cloudFiltered);
+		seg.segment (*inliers, *coefficients);
+
+		//int min_points_of_plane = 100;
+		//if (inliers->indices.size () < min_points_of_plane)  {
+		
+		// Extract the inliers
+		extract.setInputCloud (cloudFiltered);
+		extract.setIndices (inliers);
+		extract.setNegative (false);
+		// Get the points associated with the planar surface
+		extract.filter (*cloudPlane);
+		
+		// Extract points above plane, remove the planar inliers, extract the rest
+		//extract.setNegative (true);
+		//extract.filter (*cloudOnplane);
+
+
+
+
 	    
 	    // uncomment this for normal visuallization, and remove visualizer from doorHandle.cpp or use doorHandle.cpp_normalVisualisation
 		// --------------------------------------------------------
@@ -59,10 +117,12 @@ pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr Detect::startDetection(
 	    // --------------------------------------------------------
 	    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
 	    viewer->setBackgroundColor (0, 0, 0);
-	    pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGBA> rgb(cloud);
-	    viewer->addPointCloud<pcl::PointXYZRGBA> (cloud, rgb, "sample cloud");
+	    pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGBA> rgb(cloudFiltered);
+	    viewer->addPointCloud<pcl::PointXYZRGBA> (cloudFiltered, rgb, "sample cloud");
 	    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud");
-	    viewer->addPointCloudNormals<pcl::PointXYZRGBA, pcl::Normal> (cloud, cloud_normals, 10, 0.05, "normals");
+	    //cout << cloudNormalFiltered->size() ;
+	    //cout << cloudFiltered->size() ;
+	    viewer->addPointCloudNormals<pcl::PointXYZRGBA, pcl::Normal> (cloudFiltered, cloudNormalFiltered, 10, 0.05, "normals");
 	    viewer->addCoordinateSystem (0.2);
 	    viewer->initCameraParameters ();
 		
